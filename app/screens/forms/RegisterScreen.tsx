23192b0888b4d48d0a317e9e9a3b683f030a5681
Nadav Galili <nadavg1000@gmail.com>
2024-09-07 17:41:31 +0300

    trying a new google web id

--- app/screens/forms/RegisterScreen.tsx
+++ app/screens/forms/RegisterScreen.tsx
@@ -26,10 +26,13 @@ const RegisterScreen = () => {
   const [error, setError] = useState<string | null>(null);
   const [userInfo, setUserInfo] = useState<any | null>(null);
 
+  const localWebClient =
+    "642539761997-uu7qdgioieccn89baa9v8tkhc84r0nbu.apps.googleusercontent.com";
+  const googleWebClientId =
+    "642539761997-mjob5qps6oodjiq52uj7noa805seisde.apps.googleusercontent.com";
   const configureGoogleSignin = async () => {
     GoogleSignin.configure({
-      webClientId:
-        "642539761997-uu7qdgioieccn89baa9v8tkhc84r0nbu.apps.googleusercontent.com",
+      webClientId: googleWebClientId || localWebClient,
     });
   };
   useEffect(() => {
@@ -40,6 +43,7 @@ const RegisterScreen = () => {
     try {
       await GoogleSignin.hasPlayServices();
       const userInfo = await GoogleSignin.signIn();
+      console.log("🚀 ~ signIn ~ userInfo:", userInfo);
       setUserInfo(userInfo);
       setError(null);
 
