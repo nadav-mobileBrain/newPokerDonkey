2fa27f267e7d0a992b90a1e09e5a320d85b2e206
Nadav Galili <nadavg1000@gmail.com>
2024-09-07 14:10:52 +0300

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
     name: "Poker Donkey",
     slug: "newPokerDonkey",
     scheme: "newpokerdonkey",
-    version: "1.0.32",
+    version: "1.0.33",
     orientation: "portrait",
     icon: "./app/assets/appLogo.png",
     userInterfaceStyle: "light",
